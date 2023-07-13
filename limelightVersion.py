3e15d2d38e7ca308fe8eb952e40cb3ff53d6b743
Jerry <jerryfu_mc@outlook.com>
2023-07-13 19:17:42 -0400

    e

--- limelightVersion.py
+++ limelightVersion.py
@@ -7,40 +7,34 @@ def runPipeline(image, llrobot):
     img_hsv = ocv.cvtColor(image, ocv.COLOR_BGR2HSV)
     # convert the hsv to a binary image by removing any pixels
     # that do not fall within the following HSV Min/Max values
-    img_threshold = ocv.inRange(img_hsv, (18,128,128), (28, 255, 255))
+    img_threshold = ocv.inRange(img_hsv, (18, 128, 128), (28, 255, 255))
 
     # target = ocv.bitwise_and(image,image, mask=img_threshold)
 
     # find contours in the new binary image
-    contours, _ = ocv.findContours(img_threshold,
-    ocv.RETR_EXTERNAL, ocv.CHAIN_APPROX_SIMPLE)
+    contours, _ = ocv.findContours(img_threshold, ocv.RETR_EXTERNAL, ocv.CHAIN_APPROX_SIMPLE)
 
     largestContour = np.array([[]])
 
     # initialize an empty array of values to send back to the robot
-    llpython = [0,0,0,0,0,0,0,0]
+    llpython = [0, 0, 0, 0, 0, 0, 0, 0]
 
     # if contours have been detected, draw them
     if len(contours) > 0:
         ocv.drawContours(image, contours, -1, 255, 2)
         # record the largest contour
-        largestContour = max(contours, key=ocv.contourArea)
+        largestContour = max(contours, key = ocv.contourArea)
 
         # get the unrotated bounding box that surrounds the contour
-        x,y,w,h = ocv.boundingRect(largestContour)
+        x, y, w, h = ocv.boundingRect(largestContour)
 
         # draw the unrotated bounding box
-        ocv.rectangle(image,(x,y),(x+w,y+h),(0,255,255),2)
+        ocv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 255), 2)
 
         # record some custom data to send back to the robot
-        llpython = [1,x,y,w,h,9,8,7]
+        llpython = [1, x, y, w, h, 9, 8, 7]
 
     #return the largest contour for the LL crosshair, the modified image, and custom robot data
-    print("Largest Contour:"+str(largestContour))
-    print("llpython:"+str(llpython))
-       
-    # make sure to return a contour,
-    # an image to stream,
-    # and optionally an array of up to 8 values for the "llpython"
-    # networktables array
+    # make sure to return a contour, an image to stream,
+    # and optionally an array of up to 8 values for the "llpython"networktables array
     return largestContour, image, llpython
--- main.py
+++ main.py
@@ -10,41 +10,42 @@ cap = ocv.VideoCapture(1)
 while True:
     success, image = cap.read()
 
- # convert the input image to the HSV color space
+    # convert the input image to the HSV color space
     img_hsv = ocv.cvtColor(image, ocv.COLOR_BGR2HSV)
-    # convert the hsv to a binary image by removing any pixels
-    # that do not fall within the following HSV Min/Max values
-    img_threshold = ocv.inRange(img_hsv, (18,128,128), (28, 255, 255))
+    # convert the hsv to a binary image by removing any pixels that do not fall within the following HSV Min/Max values
+
+    # img_threshold = ocv.inRange(img_hsv, (18,128,128), (28, 255, 255)) # FRC yellow cone
+
+    img_threshold = ocv.inRange(img_hsv, (90, 50, 120), (150, 255, 255)) # plastic water bottle
 
     # target = ocv.bitwise_and(image,image, mask=img_threshold)
 
     # find contours in the new binary image
-    contours, _ = ocv.findContours(img_threshold,
-    ocv.RETR_EXTERNAL, ocv.CHAIN_APPROX_SIMPLE)
+    contours, _ = ocv.findContours(img_threshold, ocv.RETR_EXTERNAL, ocv.CHAIN_APPROX_SIMPLE)
 
     largestContour = np.array([[]])
 
     # initialize an empty array of values to send back to the robot
-    llpython = [0,0,0,0,0,0,0,0]
+    llpython = [0, 0, 0, 0, 0, 0, 0, 0]
 
     # if contours have been detected, draw them
     if len(contours) > 0:
         ocv.drawContours(image, contours, -1, 255, 2)
         # record the largest contour
-        largestContour = max(contours, key=ocv.contourArea)
+        largestContour = max(contours, key = ocv.contourArea)
 
         # get the unrotated bounding box that surrounds the contour
-        x,y,w,h = ocv.boundingRect(largestContour)
+        x, y, w, h = ocv.boundingRect(largestContour)
 
         # draw the unrotated bounding box
-        ocv.rectangle(image,(x,y),(x+w,y+h),(0,255,255),2)
+        ocv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 255), 2)
 
         # record some custom data to send back to the robot
-        llpython = [1,x,y,w,h,9,8,7]
+        llpython = [1, x, y, w, h, 9, 8, 7]
 
     #return the largest contour for the LL crosshair, the modified image, and custom robot data
     # print("Largest Contour:"+str(largestContour))
-    print("llpython:"+str(llpython))
+    print("llpython:" + str(llpython))
 
     currentTime = time.time()
     fps = 1 / (currentTime - previousTime)
